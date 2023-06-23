cc7f50c0a3af63df34b60f39df7a1b255ae479b5
leiah133 <leiaanjos87@gmail.com>
2023-06-22 23:11:56 -0300

    feat: ngtemplate and ngmodel

--- src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -7,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivasAtributosComponent implements OnInit {
   public valor: boolean = true;
+  public height: string = '100px';
+  public nome: string = '';
+  public list: Array<{nome: string}> = [{nome: 'Léia Oliver'}];
+
   ngOnInit(): void {
     setInterval(() =>{
       this.valor =!this.valor;
+      if(this.height == '100px'){
+        this.height = '70px';
+      } else {
+        this.height = '50px';
+      }
     },1000);
   };
 
+  public salvar(){
+    this.list.push({nome: this.nome});
+    this.nome = '';
+  }
 }
